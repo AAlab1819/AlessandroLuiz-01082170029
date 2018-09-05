fa89d37e5709134ccbad67bb5d15873a5a243418
sanddroo <42812340+sanddroo@users.noreply.github.com>
2018-09-05 22:16:33 +0700

    Update 854A.cpp

--- Week 01 - Intro/854A.cpp
+++ Week 01 - Intro/854A.cpp
@@ -4,23 +4,23 @@ using namespace std;
 
 int main()
 {
-    int number,numerator=0,demenumetor=0,divnumber;
+    int number,numerator=0,demenumetor=0,divnumber; //divnumber means a stored number after the Number divided by two
     cin>>number;
-    if(number%2==0)
+    if(number%2==0) //this line means if the number is a even number, that number will through down to that statements
     {
         divnumber=number/2;
-        if(divnumber%2==1)
+        if(divnumber%2==1)// this line means if a divnumber is a odd number, that divnumber will through down to that statements
         {
             numerator=divnumber-2;
             demenumetor=number-numerator;
         }
-        else
+        else //this line means if a divnumber is a even number, that divnumber will through down to that statements
         {
             numerator=divnumber-1;
             demenumetor=divnumber+1;
         }
     }
-    else
+    else //this line means if a number is a odd number, that number will through down to that statements
     {
         divnumber=number/2;
         numerator=divnumber;
