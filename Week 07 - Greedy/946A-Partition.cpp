fa86a17b97b7606a86c454c3c3ddc9efbbf3f649
sanddroo <42812340+sanddroo@users.noreply.github.com>
2018-10-25 00:23:19 +0700

    Update 946A-Partition.cpp

--- Week 07 - Greedy/946A-Partition.cpp
+++ Week 07 - Greedy/946A-Partition.cpp
@@ -2,21 +2,28 @@
 using namespace std;
  
 int main() {
+	//Declaring variables
 	int input, num;
 	int sum=0 ,negSum=0;
 	cin>>input;
+	//for loop to input the number
 	for(int i= 0 ; i<input ;i++)
 	{
 		cin>>num;
+		//check if the inputed number is postive number or not
 		if(num>0)
 		{
+			//sum all inputed number with the positive number
 			sum+=num;
 		}
+		////check if the inputed number is negative number or not
 		else
 		{
+			//sum all inputed number with the negative number
 			negSum+=num;
 		}
 	}
+	// calculate the sum and negsum
 	cout<<sum - negSum<<endl;
 	return 0;
 }
