7912a69808c0493985f4094275692f931148a57e
sanddroo <42812340+sanddroo@users.noreply.github.com>
2018-10-25 00:29:38 +0700

    Update 978B-FileName.cpp

--- Week 07 - Greedy/978B-FileName.cpp
+++ Week 07 - Greedy/978B-FileName.cpp
@@ -2,20 +2,25 @@
 using namespace std;
  
 int main() {
+	//Declaring variabels
 	int input,temp=0, answer=0;
 	string FileName;
 	cin>>input;
+	//for loop to input the string
 	for(int i= 0 ; i<input;i++)
 	{
 		cin>>FileName;
+		//check if there are x in the string and store in temp
 		if(FileName[i]=='x')
 		{
 			temp++;
+			//if the temp more than 2 the answer will be increase
 			if(temp>2)
 			{
 				answer++;
 			}
 		}
+		//the next inputed string is not x or there no x in inputed string the temp will be reset to 0 
 		else
 		{
 			temp=0;
